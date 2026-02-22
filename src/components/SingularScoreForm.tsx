52740f9a2d6bf08e314502e107077fdd61506eb1
carokann1945 <carokann1945@gmail.com>
2026-02-22 23:43:34 +0900

    [chore] svgr 설치

--- src/components/SingularScoreForm.tsx
+++ src/components/SingularScoreForm.tsx
@@ -1,7 +1,13 @@
 export default function SingularScoreForm() {
   return (
-    <div>
-      <h1>Singular Score Form</h1>
+    <div className="w-[288px] h-[56px] flex justify-center items-center bg-red50">
+      <div className="w-[250px] h-[23px] flex justify-between">
+        <div></div>
+        <div>
+          <p>80</p>
+          <p>/ 100</p>
+        </div>
+      </div>
     </div>
   );
 }
